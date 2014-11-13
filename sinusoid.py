5802dcdfd80c03f240acdc20f82d267c320ac748
Jose <jose@zunware.com>
2014-11-13 01:18:18 -0600

    OK

--- sinusoid.py
+++ sinusoid.py
@@ -14,4 +14,19 @@ plt.axis([-.002, .002, -.8, .8])
 plt.xlabel('time')
 plt.ylabel('amplitude')
 
+# plt.show()
+
+# Complex Sinewave
+
+N = 500
+k = 3
+n = np.arange(-N/2, N/2)
+s = np.exp(1j * 2 * np.pi * k * n / N)
+
+plt.plot(n, np.real(s))
+plt.axis([-N/2, N/2-1, -1, 1])
+plt.xlabel('n')
+plt.ylabel('amplitude')
 plt.show()
+
+
